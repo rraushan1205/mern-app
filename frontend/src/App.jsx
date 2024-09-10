e9ad8b7287ebde4350fa540f8ab4a1eda257c22a
unknown <raushanraj1205@gmail.com>
2024-09-10 09:15:55 +0530

    frontend api call changes-added cors

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,24 @@
+import { useState } from "react";
 import "./index.css";
-
+import axios from 'axios';
 function App() {
-  
+  const [res, setRes] = useState("");
+  useEffect(() => {
+    axios.get('https://mern-app-wheat-two.vercel.app/h')
+      .then(function (response) {
+        console.log(response);
+        setRes(response.data);  // Extract the data from the response
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      });
+  }, []);
   return (
+    
     <>
     <div className="w-[100%] h-screen bg-red-200 flex justify-center items-center flex-col">
-    <strong>Hello</strong>
+    <strong>{res ? res : "Loading..."}</strong> 
     <p>I am here</p>
     </div>
     </>
