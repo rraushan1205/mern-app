14f7c41efe234f3dadc7de2f6e73e4b65e662b31
unknown <raushanraj1205@gmail.com>
2024-09-10 08:17:23 +0530

    initial Frontend commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import "./index.css";
+
+function App() {
+  
+  return (
+    <>
+    <div className="w-[100%] h-screen bg-red-200 flex justify-center items-center flex-col">
+    <strong>Hello</strong>
+    <p>I am here</p>
+    </div>
+    </>
+  );
+}
+
+export default App;
